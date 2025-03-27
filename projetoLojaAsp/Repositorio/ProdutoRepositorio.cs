d11c5c8b117644a13e645f0ce3119c677d794582
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-03-27 14:37:56 -0300

    desenvolvido o UsuarioRepositorio e ProdutoRepositorio

--- projetoLojaAsp/Repositorio/ProdutoRepositorio.cs
+++ projetoLojaAsp/Repositorio/ProdutoRepositorio.cs
@@ -6,11 +6,21 @@
 
         public ProdutoRepositorio(IConfiguration configuration) => _connectionString = configuration.GetConnectionString("DefaultConnection");
 
-        public void AdicionarUsuario(Usuario usuario )
+        public void AdicionarProduto(Produto produto )
         {
-            using (var db = new )
-        
-        
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.CreateCommand();
+                cmd.CommandText = "INSERT INTO Produtos (Nome, Email, Senha) VALUES (@Nome, @Email, @Senha)";
+                cmd.Parameters.AddWithValue("@name", produto.name);
+                cmd.Parameters.AddWithValue("@description", produto.description);
+                cmd.Parameters.AddWithValue("@price", produto.price);
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+
         }
     }
 }
--- projetoLojaAsp/Repositorio/UsuarioRepositorio.cs
+++ projetoLojaAsp/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,27 @@
-﻿namespace projetoLojaAsp.Repositorio
+﻿using projetoLojaAsp.Models;
+
+namespace projetoLojaAsp.Repositorio
 {
     public class UsuarioRepositorio
     {
+        private readonly string _connectionString;
+
+        public UsuarioRepositorio(IConfiguration configuration) => _connectionString = configuration.GetConnectionString("DefaultConnection");
+        
+        public AdicionarUsuario(Usuario usuario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.CreateCommand();
+                cmd.CommandText = "INSERT INTO Usuarios (name, email, senha) VALUES (@name, @email, @password)";
+                cmd.Parameters.AddWithValue("@name", usuario.name);
+                cmd.Parameters.AddWithValue("@email", usuario.email);
+                cmd.Parameters.AddWithValue("@password", usuario.password);
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+        }
     }
 }
