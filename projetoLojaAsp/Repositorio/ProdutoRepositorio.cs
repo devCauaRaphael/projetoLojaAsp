1fe3bde867948b6bd617deb2a52e23862767db78
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-03-27 14:25:12 -0300

    inicio do desenvolvimento da classe ProdutoRepositorio e UsuarioRepositorio

--- projetoLojaAsp/Repositorio/ProdutoRepositorio.cs
+++ projetoLojaAsp/Repositorio/ProdutoRepositorio.cs
@@ -2,5 +2,8 @@
 {
     public class ProdutoRepositorio
     {
+        private readonly string _connectionString;
+
+        public ProdutoRepositorio(IConfiguration configuration) => _connectionString = configuration.GetConnectionString("DefaultConnection");
     }
 }
