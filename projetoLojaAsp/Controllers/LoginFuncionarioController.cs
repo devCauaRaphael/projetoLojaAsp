7110be07b50516272b200151aaab8add1945f579
Cauã Raphael <dev.cauaraphael@gmail.com>
2025-03-28 14:22:44 -0300

    adicionado a controller LoginFuncionario

---
+++ projetoLojaAsp/Controllers/LoginFuncionarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projetoLojaAsp.Controllers
+{
+    public class LoginFuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- projetoLojaAsp/Repositorio/FuncionarioRepositorio.cs
+++ projetoLojaAsp/Repositorio/FuncionarioRepositorio.cs
@@ -1,9 +1,29 @@
-﻿namespace projetoLojaAsp.Repositorio
+﻿using projetoLojaAsp.Models;
+
+namespace projetoLojaAsp.Repositorio
 {
     public class FuncionarioRepositorio
     {
         private readonly string _connectionString;
 
-        
+        public FuncionarioRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void AdicionarFuncionario(Funcionario funcionario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "INSERT INTO Usuario (name, email, password) VALUES (@name, @email, @password)";
+                cmd.Parameters.AddWithValue("@name", funcionario.name);
+                cmd.Parameters.AddWithValue("@email", funcionario.email);
+                cmd.Parameters.AddWithValue("@password", funcionario.password);
+                cmd.ExecuteNonQuery();
+
+
+            }
+        }
     }
 }
